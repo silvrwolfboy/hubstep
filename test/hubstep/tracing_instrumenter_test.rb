52188d29c7128675a2a7c022ef597bb7e511a128
Adam Roben <adam@roben.org>
2016-12-15 16:17:45 -0500

    Move TracingInstrumentorTest inside HubStep module

--- test/hubstep/tracing_instrumenter_test.rb
+++ test/hubstep/tracing_instrumenter_test.rb
@@ -3,50 +3,52 @@
 require_relative "../test_helper"
 require "hubstep/tracing_instrumenter"
 
-class TracingInstrumentorTest < Minitest::Test
-  def setup
-    @tracer = HubStep::Tracer.new
-    @tracer.enabled = true
-    @instrumenter = HubStep::TracingInstrumenter.new(@tracer)
-  end
-
-  def test_traces_instrumented_blocks
-    original_payload = { bar: :baz }
-    inner_payload = nil
-    span = nil
-    @instrumenter.instrument("foo", original_payload) do |payload|
-      inner_payload = payload
-      span = @tracer.bottom_span
+module HubStep
+  class TracingInstrumentorTest < Minitest::Test
+    def setup
+      @tracer = Tracer.new
+      @tracer.enabled = true
+      @instrumenter = TracingInstrumenter.new(@tracer)
     end
 
-    assert_equal original_payload, inner_payload
-    assert_equal "foo", span.operation_name
-  end
+    def test_traces_instrumented_blocks
+      original_payload = { bar: :baz }
+      inner_payload = nil
+      span = nil
+      @instrumenter.instrument("foo", original_payload) do |payload|
+        inner_payload = payload
+        span = @tracer.bottom_span
+      end
 
-  def test_passes_span_to_blocks
-    original_payload = { bar: :baz }
-    inner_payload = nil
-    inner_span = nil
-    @instrumenter.instrument("foo", original_payload) do |payload, span|
-      inner_payload = payload
-      inner_span = span
+      assert_equal original_payload, inner_payload
+      assert_equal "foo", span.operation_name
     end
 
-    assert_equal original_payload, inner_payload
-    assert_equal "foo", inner_span.operation_name
-  end
+    def test_passes_span_to_blocks
+      original_payload = { bar: :baz }
+      inner_payload = nil
+      inner_span = nil
+      @instrumenter.instrument("foo", original_payload) do |payload, span|
+        inner_payload = payload
+        inner_span = span
+      end
 
-  def test_sends_notifications
-    event = nil
-    callback = lambda do |*args|
-      event = ActiveSupport::Notifications::Event.new(*args)
-    end
-    payload = { bar: :baz }
-    ActiveSupport::Notifications.subscribed(callback, "foo") do
-      @instrumenter.instrument("foo", payload)
+      assert_equal original_payload, inner_payload
+      assert_equal "foo", inner_span.operation_name
     end
 
-    assert_equal "foo", event.name
-    assert_equal payload, event.payload
+    def test_sends_notifications
+      event = nil
+      callback = lambda do |*args|
+        event = ActiveSupport::Notifications::Event.new(*args)
+      end
+      payload = { bar: :baz }
+      ActiveSupport::Notifications.subscribed(callback, "foo") do
+        @instrumenter.instrument("foo", payload)
+      end
+
+      assert_equal "foo", event.name
+      assert_equal payload, event.payload
+    end
   end
 end
