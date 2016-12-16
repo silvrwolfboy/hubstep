d3e82176b4b5f2f5a373da10c4cd099860ea7b67
Adam Roben <adam@roben.org>
2016-12-16 14:30:55 -0500

    Docs for lib/hubstep/instrumenter.rb

--- lib/hubstep/instrumenter.rb
+++ lib/hubstep/instrumenter.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
 module HubStep
-  # Wrapper around ActiveSupport::Notifications that traces instrumented
-  # blocks.
+  # Wrapper around ActiveSupport::Notifications (or any object with the same
+  # API) that traces instrumented blocks. Behaves identically to the underlying
+  # object except that blocks passed to #instrument will be passed two
+  # arguments instead of one: the payload Hash (as usual) and the Span that
+  # will represent this block of code in the trace. The Span can be used to set
+  # tags etc.
+  #
+  #   require "hubstep/instrumenter"
+  #
+  #   instrumenter = HubStep::Instrumenter.new(tracer, ActiveSupport::Notifications)
+  #
+  #   def deflate(data)
+  #     # This block will be recorded as a span by the tracer and will also
+  #     # generate a notification as usual.
+  #     instrumenter.instrument("deflate.zlib") do |payload, span|
+  #       span.set_tag("bytesize", data.bytesize)
+  #       Zlib::Deflate.deflate(data)
+  #     end
+  #   end
   class Instrumenter
     # Creates an Instrumenter
     #
